168ff27e34fb62ed114886bbd3b77c3324519382
Nicolas <nicolasgirardet@live.fr>
2024-05-20 09:17:54 +0200

    added send_mail function

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from listings.models import Band, Listing
+from listings.forms import ContactUsForm
 
 def band_list(request):
     bands = Band.objects.all()
@@ -17,3 +19,21 @@ def listing_list(request):
 def listing_detail(request, listing_id):
     listing = Listing.objects.get(id=listing_id)
     return render(request, 'listings/listing_detail.html', {'listing': listing})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+
+            return redirect('email-sent')
+
+    else:
+        form = ContactUsForm()
+    return render(request, 'listings/contact.html', {'form': form})
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('bands/', views.band_list, name='band-list'),
     path('bands/<int:band_id>/', views.band_detail, name='band-detail'),
     path('listings/', views.listing_list, name='listing-list'),
-    path('listings/<int:listing_id>/', views.listing_detail, name='listing-detail'),    
+    path('listings/<int:listing_id>/', views.listing_detail, name='listing-detail'),  
+    path('contact-us/', views.contact, name='contact'), 
 ]
