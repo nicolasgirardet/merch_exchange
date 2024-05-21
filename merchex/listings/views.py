59f8141a538aaf1bd6a92a1c3bf96b0cf461c4c9
Nicolas <nicolasgirardet@live.fr>
2024-05-21 19:26:32 +0200

    finished implenting CRUD operations for listings and bands

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -24,8 +24,21 @@ def band_create(request):
 
 def band_update(request, band_id):
     band = Band.objects.get(id=band_id)
-    form = BandForm(instance=band)
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
     return render(request, 'listings/band_update.html', {'form': form})
+
+def band_delete(request, band_id):
+    band = Band.objects.get(id=band_id)
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+    return render(request, 'listings/band_delete.html', {'band': band})
 
 def listing_list(request):
     listings = Listing.objects.all()
@@ -44,6 +57,24 @@ def listing_create(request):
     else:
         form = ListingForm()
     return render(request, 'listings/listing_create.html', {'form': form})
+
+def listing_update(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+    return render(request, 'listings/listing_update.html', {'form': form})
+
+def listing_delete(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    return render(request, 'listings/listing_delete.html', {'listing': listing})
 
 def contact(request):
     if request.method == 'POST':
